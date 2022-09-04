cd812f4f768af580a8db32a945cea1377c14113a
gitclone93 <65633360+gitclone93@users.noreply.github.com>
2022-09-04 15:38:00 -0400

    Add files via upload

---
+++ pwroptions.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+title Power Options
+:choice
+set /P c=Select a Power option [H/S/R/L/O]. Press E to exit.
+if /I "%c%" EQU "H" goto :H
+if /I "%c%" EQU "S" goto :S
+if /I "%c%" EQU "R" goto :R
+if /I "%c%" EQU "L" goto :L
+if /I "%c%" EQU "O" goto :O
+if /I "%c%" EQU "E" goto :E
+
+
+:H
+shutdown /h
+
+:S
+shutdown /s /t 0
+
+:R
+shutdown /r /t 0
+
+:L
+rundll32.exe user32.dll,LockWorkStation
+
+:O
+logoff
+
+:E
+exit
+
+echo Thank you for using Power Options!
+echo Made by Karim A.
+
+
+
+
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Thanks for downloading Power Options!
+this took me 10 min to make
+anyway when you start the program it will say Select a Power option [H/R/S/L/O]. 
+At that screen, press H to hibernate, press S to shutdown, press R to restart, Press L to lock, and press O to log off.
+Got it? Ok.
